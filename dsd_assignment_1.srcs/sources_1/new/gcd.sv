0436e3317a37cdad26e9977a932641286acf168b
Deniz Lopes Günes <denizlg24@gmail.com>
2026-09-17 12:53:06 +0200

    feat: start implementing combinational logic

--- dsd_assignment_1.srcs/sources_1/new/gcd.sv
+++ dsd_assignment_1.srcs/sources_1/new/gcd.sv
@@ -20,8 +20,9 @@ module gcd (
     output logic          ack,    // Input received / Computation is complete.
     output logic [15 : 0] C       // The result.
 );
-    typedef enum logic [1 : 0] { ... } state_t; // Input your own state names here
-
+    typedef enum logic [2 : 0] { in_a, load_a, in_b, load_b, compare, b_greater, a_greater, print } state_t; // Input your own state names here
+    logic [1:0] FN;
+    logic Z, N;
     shortint unsigned reg_a, next_reg_a, reg_b, next_reg_b;
     
     state_t state, next_state;
@@ -29,7 +30,45 @@ module gcd (
     // Combinatorial logic
     always_comb begin
         case (state)
-            // <COMBINATORIAL BODY> 
+            in_a: begin
+                if(req == 1)
+                    next_state = load_a;
+                else
+                    next_state = in_a;
+            end
+            load_a: begin
+                next_reg_a = unsigned'(AB);
+                if (req == 0)
+                    next_state = in_b;
+                else
+                    next_state = load_a;
+            end
+            in_b: begin
+                if(req == 1)
+                    next_state = load_b;
+                else
+                    next_state = in_b;
+            end
+            load_b: begin
+                next_reg_b = unsigned'(AB);
+                next_state = compare;
+            end
+            compare: begin
+                if(Z == 1)
+                    next_state = print;
+                else begin
+                    if(N == 1)
+                        next_state = b_greater;
+                    else
+                        next_state = a_greater;
+                end   
+            end
+            print: begin
+                if(req == 0)
+                    next_state = in_a;
+                else
+                    next_state = print;
+            end
         endcase
     end
 
