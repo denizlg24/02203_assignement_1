d0fb54bc10964122dedb4acaf701e66072439efa
Rose <rosetimlin22@gmail.com>
2026-09-24 11:36:42 +0200

    hopefully working datapath

--- dsd_assignment_1.srcs/sources_1/new/datapath.sv
+++ dsd_assignment_1.srcs/sources_1/new/datapath.sv
@@ -21,13 +21,46 @@
 
 
 module datapath(
-    input [15:0] AB,
-    output [15:0] C,
-    input ABorALU,
-    input LDA,
-    input LDB,
-    output N,
-    output Z,
-    input [1:0] FN
+    input  logic [15:0] AB,
+    output logic  [15:0] C,
+    input  logic   ABorALU,
+    input  logic       LDA,
+    input  logic       LDB,
+    output logic         N,
+    output logic         Z,
+    input  logic  [1:0] FN,
+    input  logic       clk,
+    input  logic     reset
     );
+    wire [15:0] A, B, Y;
+    
+    c_alu alu(
+        .A (A),
+        .B (B),
+        .fn (FN),
+        .C (Y),
+        .Z(Z),
+        .N(N)
+    );
+    
+    c_reg reg_a(
+        .clk (clk),
+        .en (LDA),
+        .data_in (C),
+        .data_out (A)
+    );
+    c_reg reg_b(
+        .clk (clk),
+        .en (LDB),
+        .data_in (C),
+        .data_out (B)
+    );
+    
+    c_mux mux(
+        .data_in1(Y),
+        .data_in2(AB),
+        .s(ABorALU),
+        .data_out(C)
+    );
+    
 endmodule
