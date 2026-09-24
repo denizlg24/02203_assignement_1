a6d7393c789119bc4715ade89804c3fed8737738
Rose <rosetimlin22@gmail.com>
2026-09-24 11:04:40 +0200

    create boilerplate

---
+++ dsd_assignment_1.srcs/sources_1/new/fsm.sv
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/24/2026 11:01:52 AM
+// Design Name: 
+// Module Name: fsm
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fsm(
+    input req,
+    output ack,
+    output ABorALU,
+    output LDA,
+    output LDB,
+    input N,
+    input Z,
+    input [1:0] FN
+    );
+endmodule
