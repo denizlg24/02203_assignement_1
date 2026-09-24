9b10371c7c97624744572e49ced1e34619aab1ed
rusbeko <jorgesilva288cm@gmail.com>
2026-09-24 11:50:11 +0200

    Implement GCD module with FSM and datapath

--- dsd_assignment_1.srcs/sources_1/new/gcd_task4.sv
+++ dsd_assignment_1.srcs/sources_1/new/gcd_task4.sv
@@ -21,6 +21,54 @@
 
 
 module gcd_task4(
-
+    input logic             clk,
+    input logic             reset,
+    input logic             req,
+    input logic  [15 : 0]    AB,
+    output logic            ack,
+    output logic [15 : 0]   C
     );
+    
+    // shared signals between FSM and Datapath
+    logic        ABorALU;
+    logic        LDA;
+    logic        LDB;
+    logic [1:0]  FN;
+    logic        Z;
+    logic        N;
+
+    // FSM
+    fsm u_fsm (
+        // exterior
+        .clk(clk),
+        .reset(reset),
+        .req(req),
+        .ack(ack),
+        // from datapath
+        .Z(Z),
+        .N(N),
+        // to datapath
+        .ABorALU(ABorALU),
+        .LDA(LDA),
+        .LDB(LDB),
+        .FN(FN)
+    );
+
+    // Datapath
+    datapath u_datapath (
+        // from exterior
+        .clk(clk),
+        .AB(AB),
+        // to exterior (leds)
+        .C(C),
+        // from FSM
+        .ABorALU(ABorALU),
+        .LDA(LDA),
+        .LDB(LDB),
+        .FN(FN),
+        // to FSM (flags)
+        .Z(Z),
+        .N(N)
+     );
+     
 endmodule
